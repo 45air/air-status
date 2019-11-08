775f23c6def0f7a9a8ec193a8ed74feb49d7ad2b
Reilly Lowery <luckylowery@gmail.com>
2019-11-08 05:06:18 -0500

    Configurations

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# air-status
+# 45AIR Cloud Status
+
+Generates the 45AIR status page for our cloud hosting environment from incident reports in the `content` directory and deploys it to GitHub Pages.
+
+[Status Page](https://status.air-cloud.app)
+
+# Publish
+
+```bash
+npm run generate
+npm run deploy
+```
+
+# Documentation
+
+* [Statusfy](https://docs.statusfy.co/)
--- config.js
+++ config.js
@@ -1,6 +1,26 @@
 module.exports = {
   title: '45AIR Cloud Status',
   name: 'air_status',
-	description: '45AIR Cloud Status',
-	baseUrl: 'https://status.air-cloud.app'
+  description: '45AIR Cloud Status',
+  baseUrl: 'https://status.air-cloud.app',
+  defaultLocale: 'en',
+  locales: [
+    { code: 'en', iso: 'en-US', name: 'English' }
+  ],
+  content: {
+    systems: [
+      'customer-portal',
+      'gitlab',
+      'pipeline',
+      'api'
+    ]
+  },
+  webpush: {
+    onesignal: {
+      appId: '66906c90-6121-42e1-ab3a-135d1ba8f5ce'
+    }
+  },
+  support: {
+    en: 'https://www.45air.com/support',
+  }
 };
